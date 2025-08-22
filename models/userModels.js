124a6e6d9e1df154be632d0d5d2b397042217e86
Atul Kumar Jha <atuljha21542@gmail.com>
2025-08-23 01:26:37 +0530

    Created AuthController for Authorization of User

--- models/userModels.js
+++ models/userModels.js
@@ -1,3 +1,4 @@
+const { boolean } = require("joi");
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
@@ -9,6 +10,31 @@ const userSchema = mongoose.Schema({
         minLength: [5, "Email must have 5 characters"],
         lowerCase: true,
     },
-    
+    password:{
+        type: string,
+        required: [true, "Password is must"],
+        trim: true,
+        select: false,
+    },
+    verified:{
+        type: Boolean,
+        default: false,
+    },
+    verificationCodeValidation:{
+        type: Number,
+        default: false,
+    },
+    forgotPasswordCode: {
+        type: string,
+        default: false,
+    },
+    forgotPasswordCodeValidation: {
+        type: Number,
+        default: false,
+    },
 
-})
+},{
+timestamps:true
+});
+
+module.exports = mongoose.model("User", userSchema);
