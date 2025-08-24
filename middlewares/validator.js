aa7e125f18c7ddc14e75763195a5ee36abb1153f
Atul Kumar Jha <atuljha21542@gmail.com>
2025-08-24 11:41:16 +0530

    Implemented verirfication of verification code

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -27,3 +27,14 @@ exports.signinSchema = Joi.object({
     .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d){8,}$'))
 });
 
+
+exports.acceptCodeSchema = Joi.object({
+    email: Joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+        tlds: {allow: ['com', 'net']},
+    }),
+    providedCode: Joi.number().required(),
+}); 
