208e885fe5e1abc0ce81dfda2ef570990bffc418
Atul Kumar Jha <atuljha21542@gmail.com>
2025-08-24 23:12:59 +0530

    Built update Post API

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -71,4 +71,42 @@ exports.singlePost = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
+};
+
+exports.updatePost = async(req, res) => {
+    const {_id } = req.query;
+    const { title, description } = req.body;
+    const { userId } = req.user;
+    try {
+        const { error, value } = createPostSchema.validate({
+            title,
+            description,
+            userId,
+        });
+        if(error) {
+            return res
+            .status(401)
+            .json({ success: false, message: error.details[0].message})
+        }
+        const existingPost = await post.findOne ({ _id })
+        if(!existingPost) {
+            return res
+            .status(404)
+            .json({ success: false, message: 'Post Unavailable'});
+        }
+        if(existingPost.userId.toString() !== userId) {
+            return res
+            .status(403)
+            .json({ success: false, message: 'Unauthorized'});
+        }
+
+        existingPost.title = title;
+        existingPost.description = description;
+
+        const result = await existingPost.save();
+        res.status(200).json({ success: true, message: 'updated', data: result});
+
+    } catch (error) {
+        console.log(error);
+    }
 }
